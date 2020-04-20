eeba8f3a26f70a22f45568ce605e8d15229dd9fc
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-04-20 17:16:20 -0300

    feat: fix error with browser router

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import Main from './pages/Main';
 import About from './pages/About';
@@ -7,12 +7,10 @@ import News from './pages/News';
 
 export default function Routes() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={Main} />
-        <Route path="/about" component={About} />
-        <Route path="/news" component={News} />
-      </Switch>
-    </BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={Main} />
+      <Route path="/about" component={About} />
+      <Route path="/news" component={News} />
+    </Switch>
   );
 }
